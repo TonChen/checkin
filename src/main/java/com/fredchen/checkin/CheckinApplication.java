ede6ef332579f4f0a2c7b57b3f587fa4a0a39e11
test <test>
2018-01-19 18:25:31 +0800

    update

--- src/main/java/com/fredchen/checkin/CheckinApplication.java
+++ src/main/java/com/fredchen/checkin/CheckinApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheckinApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CheckinApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CheckinApplication.class, args);
+    }
 }
