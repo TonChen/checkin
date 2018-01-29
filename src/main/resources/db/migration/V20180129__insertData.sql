712a0efd8c66688ccc1fbda36daf9c39759fefc9
test <test>
2018-01-29 14:57:14 +0800

    update

---
+++ src/main/resources/db/migration/V20180129__insertData.sql
@@ -0,0 +1,2 @@
+--INSERT INTO `department` (`id`, `create_time`, `is_del`, `description`, `name`) VALUES ('1', '2018-01-16 11:26:11', '\0', 'miaoshu', '开发部');
+--INSERT INTO `staff` (`id`, `create_time`, `is_del`, `address`, `check_in_time`, `description`, `is_absence`, `name`, `sex`, `telephone`, `dep_id`) VALUES ('1', '2018-01-16 11:25:27', '\0', '天府大道', '2018-01-16 11:25:42', '描述', '\0', '张三', '0', '11111111', '1');
